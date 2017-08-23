ec5c9cda3f75d06a22b3879024cdf20e2f46738e
Ian <Iokeef@gmail.com>
2017-08-23 15:56:00 -0400

    init

--- TourOfHeroes/src/app/app.component.ts
+++ TourOfHeroes/src/app/app.component.ts
@@ -1,28 +1,10 @@
 import { Component } from '@angular/core';
 
-export class hero {
-  id: number;
-  name: string;
-}
-
 @Component({
-  selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name"{{hero.name}}
-  </div>
-  `,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
-  title = 'Tour of Heroes';
-
-  hero: Hero = {
-  id: 1;
-  name: 'Power Fellow';
-  };
+export class AppComponent {
+  title = 'app';
 }
