43796e50c377bceb2a4d27239f670c0a9edadfef
Ian <Iokeef@gmail.com>
2017-08-28 15:16:42 -0400

    add styling

--- TourOfHeroes/src/app/app.component.ts
+++ TourOfHeroes/src/app/app.component.ts
@@ -5,28 +5,50 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
+
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
   <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div>ID: {{hero.id}}</div>
-  <div>
-    <label>Name:
-    <input [(ngModel)]="hero.name" placeholder = "Name">
-    </label>
+  <h2>Hero List</h2>
+  <ul class="heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+      <span class="badge">{{hero.id}}.</span> {{hero.name}}
+    </li>
+  </ul>
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div>ID: {{selectedHero.id}}</div>
+    <div>
+      <label>Name:
+      <input [(ngModel)]="selectedHero.name" placeholder = "Name">
+      </label>
+    </div>
   </div>
-  // <div>Name: {{hero.name}}</div>
   `,
   styleUrls: ['./app.component.css']
-
 })
 
 export class AppComponent {
   title = 'Tour Of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
